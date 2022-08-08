658e02179c607852ece10011bc6af39bb10b2c91
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2022-08-08 14:57:14 -0300

    create auth method

--- steps/auth.py
+++ steps/auth.py
@@ -1,9 +1,31 @@
 import os
+from time import sleep
 from dotenv import load_dotenv
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+from utils.selenium import check_exists_by_name, wait_element
 
 load_dotenv()
 
 BASE_URL = os.getenv('BASE_URL')
+USER = os.getenv('USER')
+PASS = os.getenv('PASS')
 
-def auth(driver) -> None:
-    print(BASE_URL)
+def auth(driver: WebDriver) -> None:
+    driver.get(BASE_URL)
+    wait_element(
+        lambda: check_exists_by_name(driver, 'login'),
+        20,
+        'Bloqueado no login!'
+    )
+
+    loginInput = driver.find_element_by_name('login')
+    passInput = driver.find_element_by_name('password')
+    buttonSubmit = driver.find_element_by_xpath('//button')
+
+    loginInput.send_keys(USER)
+    passInput.send_keys(PASS)
+    sleep(1)
+    buttonSubmit.click()
+
+    print('passou')
