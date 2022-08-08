93eb6f6d142c42550373ee97a6dd5dfc17cacdac
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2022-08-08 14:38:43 -0300

    init auth step

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from selenium import webdriver
 from options import set_chrome_options
+from steps.auth import auth
 
 capabilities = webdriver.DesiredCapabilities.CHROME
 chrome_options = set_chrome_options()
 driver = webdriver.Chrome(options=chrome_options, desired_capabilities=capabilities, executable_path='chromedriver.exe')
 
-
+try:
+    auth(driver)
+except:
+    print("Houve um erro na autenticação!")
+    exit(1)
 
 driver.quit()
+exit(0)
