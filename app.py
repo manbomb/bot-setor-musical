742dbcb334be8a2d8597f199f1af087084526e5d
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2022-08-10 22:55:24 -0300

    app first implement one user

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
+import csv
 
 from options import set_chrome_options
 from steps.accessNewUser import accessNewUserPage
 from steps.auth import auth
+from steps.fillWithUser import fillWithUser
 
 capabilities = webdriver.DesiredCapabilities.CHROME
 chrome_options = set_chrome_options()
@@ -12,16 +14,22 @@ try:
     auth(driver)
 except:
     print("Houve um erro na autenticação!")
-    exit(1)
     driver.quit()
+    exit(1)
 
 try:
     accessNewUserPage(driver)
 except Exception as E:
     print("Houve um erro ao acessar página de usuários!")
-    print(E)
-    exit(1)
     driver.quit()
+    exit(1)
+
+try:
+    fillWithUser(driver, 'Sérgio Lucas Avilla da Silva', 'sergiolucasavilladasilva@gmail.com', 'ENCARREGADOS / SECRETÁRIOS', 'avenida')
+except Exception as E:
+    print("Houve um erro ao preencher/cadastrar usuário!")
+    driver.quit()
+    exit(1)
 
 driver.quit()
 exit(0)
