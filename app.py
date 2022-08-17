6c6ccfba757d6bf3c7f4241ad032f4a07aa2e813
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2022-08-17 07:27:16 -0300

    add access new page to for:users

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from steps.accessNewUser import accessNewUserPage
 from steps.auth import auth
 from steps.fillWithUser import fillWithUser
 from steps.readCsv import readCsv
+from utils.log import errorLogUser
 
 users = readCsv('./data.csv')
 
@@ -21,19 +22,20 @@ except:
     driver.quit()
     exit(1)
 
-try:
-    accessNewUserPage(driver)
-except Exception as E:
-    print("Houve um erro ao acessar página de usuários!")
-    driver.quit()
-    exit(1)
 
 for user in users:
+    try:
+        accessNewUserPage(driver)
+    except Exception as E:
+        print("Houve um erro ao acessar página de usuários!")
+        driver.quit()
+        exit(1)
     try:
         fillWithUser(driver, **user)
         sleep(3)
         accessNewUserPage(driver)
     except Exception as E:
+        errorLogUser(**user)
         print("Houve um erro ao preencher/cadastrar usuário!")
 
 driver.quit()
